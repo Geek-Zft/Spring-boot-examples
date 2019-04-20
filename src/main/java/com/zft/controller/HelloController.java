180d394a0df8bf649ba6fd764653da9d9cadff5d
fengtan.zhang <fengtan_zhang@sina.com>
2019-04-20 16:38:16 +0800

    统一异常处理

--- src/main/java/com/zft/controller/HelloController.java
+++ src/main/java/com/zft/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.zft.controller;
 
+import com.zft.exception.MyException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,25 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         return "hello world";
+    }
+
+    /**
+     * 自定义错误页面
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/hello1")
+    public String hello1() throws Exception {
+        throw new Exception("发生错误");
+    }
+
+    /**
+     * 自定义错误返回json格式
+     * @return
+     * @throws MyException
+     */
+    @RequestMapping("/hello2")
+    public String hello2() throws MyException {
+        throw new MyException("发生错误");
     }
 }
