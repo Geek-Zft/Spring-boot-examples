75f2c3b97ee4ef0c93f6a7d9df068407d7a0dd4d
fengtan.zhang <fengtan_zhang@sina.com>
2019-04-20 17:36:36 +0800

    使用aop处理web请求日志

--- src/main/java/com/zft/controller/HelloController.java
+++ src/main/java/com/zft/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.zft.controller;
 
 import com.zft.exception.MyException;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +32,11 @@ public class HelloController {
     @RequestMapping("/hello2")
     public String hello2() throws MyException {
         throw new MyException("发生错误");
+    }
+
+
+    @RequestMapping("/hello3")
+    public String hello3(@RequestParam String name) {
+        return "hello" + name;
     }
 }
